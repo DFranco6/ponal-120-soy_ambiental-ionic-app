ebc0909895a94790a509b6497764f2108f1c9b1d
DFranco6 <dany.franco6424@correo.policia.gov.co>
2018-08-08 16:35:56 -0500

    configuraciones land page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AboutPage } from '../about/about';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,5 +10,8 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+  // creacion de enlace Boton para ir a otra pagina
+  continua(){
+    this.navCtrl.push(AboutPage);
+  }
 }
